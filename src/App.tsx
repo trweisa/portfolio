41bc9572697f3fd1662a183e7f9b5d76b902642b
Tom Weisansal <trweisa@gmail.com>
2023-03-07 09:44:45 -0500

    Clean up header, groundwork for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<Home />} />
         <Route path="projects" element={<Projects />} />
       </Routes>
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import useMediaQuery from "@mui/material/useMediaQuery";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
@@ -8,26 +9,42 @@ function Header() {
     setSelected(divNum);
   };
 
+  const isMobile = useMediaQuery("(max-width: 800px)");
+  console.log(isMobile);
+
   return (
-    <div className="header">
-      <div className="links">
-        <Link
-          to="/"
-          className="header-item"
-          id={selected === 1 ? "active" : ""}
-          onClick={handleClick(1)}
-        >
-          About
-        </Link>
-        <Link
-          to="/projects"
-          className="header-item"
-          id={selected === 2 ? "active" : ""}
-          onClick={handleClick(2)}
-        >
-          Projects
-        </Link>
-      </div>
+    <div className="header-container">
+      {isMobile ? (
+        <div></div>
+      ) : (
+        <div className="header-desktop">
+          <div className="header-logo-container">
+            <div className="header-logo-img"></div>
+            <Link to="/" className="header-name" onClick={handleClick(1)}>
+              TOM WEISANSAL
+            </Link>
+          </div>
+          <div className="spacer"></div>
+          <div className="nav-buttons">
+            <Link
+              to="/"
+              className="header-item"
+              id={selected === 1 ? "active" : ""}
+              onClick={handleClick(1)}
+            >
+              ABOUT
+            </Link>
+            <Link
+              to="/projects"
+              className="header-item"
+              id={selected === 2 ? "active" : ""}
+              onClick={handleClick(2)}
+            >
+              PROJECTS
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
