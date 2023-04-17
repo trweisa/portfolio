0122cf4c799c3412b0fa742903f0f16d6758d968
Tom <trweisa@gmail.com>
2023-04-17 14:38:30 -0400

    Basic socials bar
    Decided to go for contact page for now

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.scss";
+import LinkBar from "../LinkBar/LinkBar";
 
 function Home() {
   return (
@@ -7,12 +8,13 @@ function Home() {
       <h1 className="home-title">Hi! I'm Tom, welcome to my page</h1>
       <h3 className="home-details">
         <div className="first-line">
-          I'm a full stack developer with a focus in React and .net/C#.  
+          I'm a full stack developer with a focus in React and .NET/C#.  
         </div>
         <div className="second-line">
           When I'm not actively coding you can find me plucking a guitar, gaming, and rock climbing.
         </div>
       </h3>
+      <LinkBar/>
     </div>
   );
 }
---
+++ src/components/LinkBar/LinkBar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./LinkBar.scss";
+
+function LinkBar() {
+  return (
+    <div className="linkbar-container">
+        <div className="linkbar">
+            <div className="icon-link">fdsaf</div>
+            <div className="icon-link">fdsaf</div>
+            <div className="icon-link">fdsaf</div>
+            <div className="icon-link">fdsaf</div>
+        </div>
+    </div>
+  );
+}
+
+export default LinkBar;
